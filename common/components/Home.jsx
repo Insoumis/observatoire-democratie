73742d05a69aeaa1cc7094b9701cdf662f64ca72
Chloé Gence <chloe.gence@gmail.com>
2017-11-19 14:21:44 +0100

    readd wordcloud

--- common/components/Home.jsx
+++ common/components/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import css from './Home.scss';
+
 const Home = () => (
-  <div>ceci est une super home !</div>
+  <div className={`container ${css.module}`}>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ducimus amet magnam sint tempora corporis distinctio? Ipsa ipsam numquam magnam maiores praesentium autem. Alias sed hic nesciunt asperiores possimus neque.
+    </p>
+    <div>
+      <div>scrutins</div>
+      <aside>
+        <div>groupes</div>
+      </aside>
+    </div>
+  </div>
 );
 
 export default Home;

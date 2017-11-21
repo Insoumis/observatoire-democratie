85ccd73398dcd0401e01d0522d2ff9798657c3bb
Chloé Gence <chloe.gence@gmail.com>
2017-11-22 00:42:30 +0100

    update image share

--- common/components/App.jsx
+++ common/components/App.jsx
@@ -21,8 +21,13 @@ const App = ({ children }) => (
       <meta property="og:description" content="L'Observatoire de la démocratie est une initiative citoyenne présentant l'activité des parlementaires à l'Assemblée nationale. Un outil simple pour comprendre une démocratie complexe." />
       <meta property="og:image" content={`${BASE_URL}${share}`} />
       <meta property="og:url" content={BASE_URL} />
-      <meta name="twitter:image:alt" content="Un outil simple pour comprendre une démocratie complexe." />
+      <meta property="og:site_name" content="Observatoire de la Démocratie" />
+      <meta name="twitter:title" content="Observatoire de la Démocratie" />
+      <meta name="twitter:description" content="L'Observatoire de la démocratie est une initiative citoyenne présentant l'activité des parlementaires à l'Assemblée nationale. Un outil simple pour comprendre une démocratie complexe." />
+      <meta name="twitter:image" content={`${BASE_URL}${share}`} />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@Obs_Democratie" />
+      <meta name="twitter:image:alt" content="Un outil simple pour comprendre une démocratie complexe" />
     </Helmet>
 
     <Header />

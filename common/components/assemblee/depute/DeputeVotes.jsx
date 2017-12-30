c3249f8f1ab5ce2de994c5051a5959b507d37834
local <chloe.gence@gmail.com>
2017-12-30 01:36:30 +0100

    remove absent stat on vote

--- common/components/assemblee/depute/DeputeVotes.jsx
+++ common/components/assemblee/depute/DeputeVotes.jsx
@@ -23,53 +23,50 @@ const DeputeVotes = ({ depute }) => {
       <h2>Votes aux scrutins publics</h2>
       <div className={css.stats}>
         <div>
-          <h3>Participation</h3>
-          <Gauge
-            picto="participation"
-            number={formatNbr(depute.stats.positions.exprimes)}
-          />
-        </div>
-        <div>
-          <h3>Absence</h3>
-          <Gauge
-            picto="absent"
-            number={formatNbr(depute.stats.positions.absent)}
-          />
-        </div>
-        <div>
-          <h3>Contre son groupe</h3>
-          {(depute.groupe_abrev === 'NI') ?
-            <div className={css.notAvailable}>Non disponible</div>
-            :
+          <div>
+            <h3>Participation</h3>
             <Gauge
-              picto="dissidence"
-              number={formatNbr(depute.stats.positions.dissidence)}
+              picto="participation"
+              number={formatNbr(depute.stats.positions.exprimes)}
             />
-          }
+          </div>
+          <div>
+            <h3>Contre son groupe</h3>
+            {(depute.groupe_abrev === 'NI') ?
+              <div className={css.notAvailable}>Non disponible</div>
+              :
+              <Gauge
+                picto="dissidence"
+                number={formatNbr(depute.stats.positions.dissidence)}
+              />
+            }
+          </div>
         </div>
         <div>
-          <h3>Vote pour</h3>
-          <Gauge
-            picto="pour"
-            color="#25a87e"
-            number={formatNbr(depute.stats.positions.pour)}
-          />
-        </div>
-        <div>
-          <h3>Vote contre</h3>
-          <Gauge
-            picto="contre"
-            color="#E23D21"
-            number={formatNbr(depute.stats.positions.contre)}
-          />
-        </div>
-        <div>
-          <h3>Abstention</h3>
-          <Gauge
-            picto="abstention"
-            color="#213558"
-            number={formatNbr(depute.stats.positions.abstention)}
-          />
+          <div>
+            <h3>Vote pour</h3>
+            <Gauge
+              picto="pour"
+              color="#25a87e"
+              number={formatNbr(depute.stats.positions.pour)}
+            />
+          </div>
+          <div>
+            <h3>Vote contre</h3>
+            <Gauge
+              picto="contre"
+              color="#E23D21"
+              number={formatNbr(depute.stats.positions.contre)}
+            />
+          </div>
+          <div>
+            <h3>Abstention</h3>
+            <Gauge
+              picto="abstention"
+              color="#213558"
+              number={formatNbr(depute.stats.positions.abstention)}
+            />
+          </div>
         </div>
       </div>
 
--- common/components/assemblee/groupe/GroupeVotes.jsx
+++ common/components/assemblee/groupe/GroupeVotes.jsx
@@ -12,53 +12,50 @@ const GroupeVotes = ({ groupe }) => (
     <h2>Votes aux scrutins publics (moyenne)</h2>
     <div className={css.stats}>
       <div>
-        <h3>Participation</h3>
-        <Gauge
-          picto="participation"
-          number={formatNbr(groupe.stats.positions.exprimes)}
-        />
-      </div>
-      <div>
-        <h3>Absence</h3>
-        <Gauge
-          picto="absent"
-          number={formatNbr(groupe.stats.positions.absent)}
-        />
-      </div>
-      <div>
-        <h3>Contre son groupe</h3>
-        {(groupe.groupe_abrev === 'NI') ?
-          <div className={css.notAvailable}>Non disponible</div>
-          :
+        <div>
+          <h3>Participation</h3>
           <Gauge
-            picto="dissidence"
-            number={formatNbr(groupe.stats.positions.dissidence)}
+            picto="participation"
+            number={formatNbr(groupe.stats.positions.exprimes)}
           />
-        }
+        </div>
+        <div>
+          <h3>Contre son groupe</h3>
+          {(groupe.groupe_abrev === 'NI') ?
+            <div className={css.notAvailable}>Non disponible</div>
+            :
+            <Gauge
+              picto="dissidence"
+              number={formatNbr(groupe.stats.positions.dissidence)}
+            />
+          }
+        </div>
       </div>
       <div>
-        <h3>Vote pour</h3>
-        <Gauge
-          picto="pour"
-          color="#25a87e"
-          number={formatNbr(groupe.stats.positions.pour)}
-        />
-      </div>
-      <div>
-        <h3>Vote contre</h3>
-        <Gauge
-          picto="contre"
-          color="#E23D21"
-          number={formatNbr(groupe.stats.positions.contre)}
-        />
-      </div>
-      <div>
-        <h3>Abstention</h3>
-        <Gauge
-          picto="abstention"
-          color="#213558"
-          number={formatNbr(groupe.stats.positions.abstention)}
-        />
+        <div>
+          <h3>Vote pour</h3>
+          <Gauge
+            picto="pour"
+            color="#25a87e"
+            number={formatNbr(groupe.stats.positions.pour)}
+          />
+        </div>
+        <div>
+          <h3>Vote contre</h3>
+          <Gauge
+            picto="contre"
+            color="#E23D21"
+            number={formatNbr(groupe.stats.positions.contre)}
+          />
+        </div>
+        <div>
+          <h3>Abstention</h3>
+          <Gauge
+            picto="abstention"
+            color="#213558"
+            number={formatNbr(groupe.stats.positions.abstention)}
+          />
+        </div>
       </div>
     </div>
   </div>
